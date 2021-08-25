14243b5892730606c841c788570a43ff9cf1fd2b
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2021-08-25 09:07:08 -0400

    initial setup for Find

--- Program.cs
+++ Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LearnRecords
 {
     class Program
     {
-        static void Main(string[] args)
+        public record Person(string FirstName, string LastName)
         {
-            Console.WriteLine("Hello World!");
+            public string[] PhoneNumbers { get; init; }
+        }
+
+        public List<Person> People {get;set;} = new List<Person>();
+
+
+        public static void Main()
+        {
+
+            
+              //SSRSReports.ReportListValue.RemoveAll(e => e.Hidden == true);
+            
+            
+            string dog = "5";
+            string cat = "7";
+
+            Console.WriteLine(dog == cat); // output: ?
+            Console.WriteLine(ReferenceEquals(dog, cat)); // output: ?
+
+            //People 
+
+            Person person1 = new("Nancy", "Davolio") { PhoneNumbers = new string[1] };
+            Console.WriteLine(person1);
+            // output: Person { FirstName = Nancy, LastName = Davolio, PhoneNumbers = System.String[] }
+
+            Person person2 = person1 with { FirstName = "John" };
+            Console.WriteLine(person2);
+            // output: Person { FirstName = John, LastName = Davolio, PhoneNumbers = System.String[] }
+            Console.WriteLine(person1 == person2); // output: False
+
+            person2 = person1 with { PhoneNumbers = new string[1] };
+            Console.WriteLine(person2);
+            // output: Person { FirstName = Nancy, LastName = Davolio, PhoneNumbers = System.String[] }
+            Console.WriteLine(person1 == person2); // output: False
+
+            person2 = person1 with { };
+            Console.WriteLine(person1 == person2); // output: True
         }
     }
 }
