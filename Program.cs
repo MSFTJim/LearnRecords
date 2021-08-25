2edb1536619f5457ee7775c1fec06e1634fa7382
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2021-08-25 08:34:40 -0400

    create console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
